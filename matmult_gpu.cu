5ad89e38007a7edbbdf9e6d354ca66224ead8890
sometimes1124 <sora1124@mbn.nifty.com>
2016-07-14 19:43:38 +0900

    first commit

--- matmult_gpu.cu
+++ matmult_gpu.cu
@@ -1,27 +1,36 @@
-#define MAT_SIZE 32 
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#define VEC_SIZE 10
+#define BLOCK_SIZE 16
 
-__global__ void MatrixMult(float *, float *, float *);
+double gettimeofday_sec();
+
+__global__ void VecAddKernel(float *, float *, float *);
 
 int main(){
+  double t1, t2;
   float *A, *B, *C;
-  cudaMallocHost((void**)&A ,sizeof(float)*MAT_SIZE*MAT_SIZE);
-  cudaMallocHost((void**)&B ,sizeof(float)*MAT_SIZE*MAT_SIZE);
-  cudaMallocHost((void**)&C ,sizeof(float)*MAT_SIZE*MAT_SIZE);
+  cudaMallocHost((void**)&A ,sizeof(float)*VEC_SIZE);
+  cudaMallocHost((void**)&B ,sizeof(float)*VEC_SIZE);
+  cudaMallocHost((void**)&C ,sizeof(float)*VEC_SIZE);
 
   float *d_A, *d_B, *d_C;
-  cudaMalloc((void**)&d_A, sizeof(float)*MAT_SIZE*MAT_SIZE);
-  cudaMalloc((void**)&d_B, sizeof(float)*MAT_SIZE*MAT_SIZE);
-  cudaMalloc((void**)&d_C, sizeof(float)*MAT_SIZE*MAT_SIZE);
+  cudaMalloc((void**)&d_A, sizeof(float)*VEC_SIZE);
+  cudaMalloc((void**)&d_B, sizeof(float)*VEC_SIZE);
+  cudaMalloc((void**)&d_C, sizeof(float)*VEC_SIZE);
 
-  cudaMemcpy(d_A, A, sizeof(float)*MAT_SIZE*MAT_SIZE, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_B, B, sizeof(float)*MAT_SIZE*MAT_SIZE, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_A, A, sizeof(float)*VEC_SIZE, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_B, B, sizeof(float)*VEC_SIZE, cudaMemcpyHostToDevice);
 
-  dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-  dim3 dimGrid(MAT_SIZE/4, MAT_SIZE/4);
-  MatrixMult<<<dimGrid, dimBlock>>>(d_A, d_B, d_C);
-
-  cudaMemcpy(C, d_C, sizeof(float)*MAT_SIZE*MAT_SIZE, cudaMemcpyDeviceToHost);
-
+  dim3 dimBlock(BLOCK_SIZE, 1);
+  dim3 dimGrid(VEC_SIZE/BLOCK_SIZE, 1);
+  VecAddKernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C);
+  
+  t1 = gettimeofday_sec();
+  cudaMemcpy(C, d_C, sizeof(float)*VEC_SIZE, cudaMemcpyDeviceToHost);
+  t2 = gettimeofday_sec();
+  
   cudaFree(d_A);
   cudaFree(d_B);
   cudaFree(d_C);
@@ -29,16 +38,18 @@ int main(){
   cudaFreeHost(A);
   cudaFreeHost(B);
   cudaFreeHost(C);
+  printf("Run Time: %f[s]", t2 - t1);
 }
 
-__global__ void MatrixMult(float *d_A, float *d_B, float *d_C) {
-  int i;
+__global__ void VecAddKernel(float *d_A, float *d_B, float *d_C) {
+  
+  int tid = blockIdx.x * blockDim.x + threadIdx.x;
 
-  unsigned int row_idx = blockDim.x * blockIdx.x + threadIdx.x;
-  unsigned int col_idx = blockDim.y * blockIdx.y * threadIdx.y;
+  d_C[tid] = d_A[tid] + d_B[tid];
+}
 
-
-  for(i = 0, i < MAT_SIZE; i++) {
-     
-
+double gettimeofday_sec(){
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec + (double)tv.tv_usec*1e-6;
 }
